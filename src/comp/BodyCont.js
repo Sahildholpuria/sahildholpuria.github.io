b0d114a7167d05e58105aa922c117b41220778cc
sahildholpuria <dholpuria1999@gmail.com>
2021-10-28 19:54:46 +0530

    commit

--- src/comp/BodyCont.js
+++ src/comp/BodyCont.js
@@ -4,7 +4,7 @@ export default function BodyCont() {
     return (
         <>
             <div class="container">
-                <img src="https://github.com/Sahildholpuria/images/blob/main/underconstruction.gif?raw=true" width="400" height="400" class="position-absolute top-50 start-50 translate-middle" alt="Under Construction"></img>
+                <img src="https://github.com/Sahildholpuria/images/blob/main/underconstruction.gif?raw=true" width="400" height="400" class="position-absolute top-50 start-50 translate-middle img-fluid" alt="Under Construction"></img>
             </div>
         </>
     )
