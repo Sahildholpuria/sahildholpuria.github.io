6e1b9a60459d8fa3dd94c82d435f550a55611633
sahildholpuria <dholpuria1999@gmail.com>
2021-10-28 19:42:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import NavBar from './comp/NavBar';
 import BodyCont from './comp/BodyCont';
--- src/comp/NavBar.js
+++ src/comp/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
         <div>
             <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="https://sahildholpuria.github.io">
       <img src="https://github.com/Sahildholpuria/images/blob/main/logo1.png?raw=true" alt="" width="50" height="50" class="d-inline-block align-text-bottom"/>
       _&gt;SahilDholpuria
     </a>
