f00db278ef2915d40056f09b7db2a01e7ce0ddc8
sahildholpuria <dholpuria1999@gmail.com>
2021-11-29 18:44:50 +0530

    bug fix

--- src/comp/CardItems.js
+++ src/comp/CardItems.js
@@ -3,13 +3,14 @@ import React from 'react'
 export default function CardItems() {
     return (
         <>
-        <div class="row">
+        <div className="container-fluid">
+        <div class="row ms-5 me-5">
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        {/* <a href="" class="btn btn-primary">Go somewhere</a> */}
+        <a href="" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
@@ -18,11 +19,12 @@ export default function CardItems() {
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        {/* <a href="" class="btn btn-primary">Go somewhere</a> */}
+        <a href="" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
 </div>
+            </div>
         </>
     )
 }
