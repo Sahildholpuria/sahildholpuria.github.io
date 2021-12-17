1e9f9e418fc23f87cd34321347b63a60179b9899
sahildholpuria <dholpuria1999@gmail.com>
2021-12-17 10:56:31 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './comp/NavBar';
 import FooterBar from './comp/FooterBar';
 import Content from './comp/Content';
+// import UnderContructionPage from './comp/UnderContructionPage';
 
 function App() {
   return (
